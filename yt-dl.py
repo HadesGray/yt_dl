eef1ef1d451af1ffa7d4d6291476be1fb169e423
HadesGray <hadesgray@163.com>
2022-04-24 09:47:35 +0800

    Create yt-dl.py

---
+++ yt-dl.py
@@ -0,0 +1,55 @@
+import os
+import yt_dlp
+
+list = {
+    #make dic include the url
+}
+
+def format_selector(ctx):
+    """ Select the best video and the best audio that won't result in an mkv.
+    NOTE: This is just an example and does not handle all cases """
+
+    # formats are already sorted worst to best
+    formats = ctx.get('formats')[::-1]
+
+    # acodec='none' means there is no audio
+    best_video = next(f for f in formats
+                      if f['vcodec'] != 'none' and f['acodec'] == 'none')
+
+    # find compatible audio extension
+    audio_ext = {'mp4': 'm4a', 'webm': 'webm'}[best_video['ext']]
+    # vcodec='none' means there is no video
+    best_audio = next(f for f in formats if (
+        f['acodec'] != 'none' and f['vcodec'] == 'none' and f['ext'] == audio_ext))
+
+    # These are the minimum required fields for a merged format
+    yield {
+        'format_id': f'{best_video["format_id"]}+{best_audio["format_id"]}',
+        'ext': best_video['ext'],
+        'requested_formats': [best_video, best_audio],
+        # Must be + separated list of protocols
+        'protocol': f'{best_video["protocol"]}+{best_audio["protocol"]}'
+    }
+
+
+ydl_opts = {
+    'format': "mp4",  #select foramt
+}
+
+dir=os.getcwd()
+
+for x,y in list.items():
+    #print(x)
+    if not os.path.exists(x):
+        os.mkdir(x)
+
+    os.chdir(dir+'\\'+x)
+    print("Current working directory: {0}".format(os.getcwd()))
+    
+    for i in list[x]:
+        print(i)
+        try:
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([i])
+        except:
+            continue
